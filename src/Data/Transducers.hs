65e7e3a7c5f58e43227274dd9df679902ce26f4f
Philipp Gabler <phips.gabler@gmail.com>
2016-01-06 21:04:53 +0100

    A working implementation, I can't believe it...

--- src/Data/Transducers.hs
+++ src/Data/Transducers.hs
@@ -1,7 +1,79 @@
-{-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE RankNTypes, TypeFamilies, MultiParamTypeClasses, Arrows#-}
+{-# LANGUAGE LambdaCase #-}
 
 module Data.Transducers (
-  Transducer
+    Transducer (..)
+  , mapping
+  , filtering
+  , concatenating
+  , flatmapping
+  , Transducible
+  , MonoTransducible
+  , toList
   ) where
 
-newtype Transducer a b = Transducer (forall r. (r -> a -> r) -> (r -> b -> r))
+import Prelude hiding ((.), id, Functor(..))
+import Control.Category
+import Control.Arrow
+import Data.Profunctor
+
+newtype Transducer a b = Transducer (forall r. (b -> r -> r) -> (a -> r -> r))
+
+instance Category Transducer where
+  id = Transducer (\cons -> cons)
+  (Transducer t1) . (Transducer t2) =  Transducer (\cons -> t2 . t1 $ cons)
+
+instance Arrow Transducer where
+  arr f = mapping f
+  first (Transducer t) = Transducer (\cons -> \(b,d) r -> let cons' = t $ \c r -> cons (c, d) r
+                                                          in cons' b r)
+
+instance ArrowChoice Transducer where
+  left (Transducer t) = Transducer (\cons ->
+                                     \case Left b -> \r -> let cons' = t $ \c r -> cons (Left c) r
+                                                           in cons' b r
+                                           Right d -> \r -> cons (Right d) r)
+
+-- unnecessary in theory (cf. WrappedArrow), but instructional.
+instance Profunctor Transducer where
+  lmap f (Transducer t) = Transducer (\cons -> \a r -> (t cons) (f a) r)
+  rmap f (Transducer t) = Transducer (\cons -> \a r -> (t $ \b -> cons (f b)) a r )
+
+
+
+
+mapping :: (a -> b) -> Transducer a b
+mapping f = Transducer (\cons -> \a r -> cons (f a) r)
+
+filtering :: (a -> Bool) -> Transducer a a
+filtering p = Transducer (\cons -> \a r -> if p a then cons a r else r)
+
+concatenating :: Foldable t => Transducer (t a) a
+concatenating = Transducer (\cons -> \as r -> foldr cons r as)
+
+flatmapping :: Foldable t => (a -> t b) -> Transducer a b
+flatmapping f = mapping f >>> concatenating
+
+
+
+
+class MonoTransducible t where
+  type Elem t :: *
+  monoTransduce :: (Elem t ~ a) => Transducer a a -> t -> t
+
+class Transducible t where
+  transduceWith :: Transducer a b -> (b -> r -> r) -> r -> t a -> r
+
+instance Transducible [] where
+  transduceWith (Transducer t) f z x = foldr (t f) z x
+  
+toList :: Transducer a b -> [a] -> [b]
+toList t xs = transduceWith t (:) [] xs
+
+
+
+
+
+
+
+
--- test/Data/Transducers/TransducersSpec.hs
+++ test/Data/Transducers/TransducersSpec.hs
@@ -5,8 +5,30 @@ import Test.QuickCheck
 
 import Data.Transducers
 
--- `main` is here so that this module can be run from GHCi on its own.  It is
--- not needed for automatic spec discovery.
+-- LIST TESTS
+list_test1 = toList ((arr (+1) >>> arr show) &&& arr show) [1..5]
+
+list_test2 = (`toList` [1..5]) $ proc x -> do
+  y <- arr show <<< arr (+1) -< x
+  z <- arr show -< x
+  returnA -< y ++ z
+
+list_test3 = (`toList` [1..5]) $ proc x -> do
+  y <- if even x
+       then returnA -< x `div` 2
+       else returnA -< x * 10
+  filtering (<20) -< y
+
+list_test4 = toList (flatmapping (\x -> [x, x])) [1..5]
+
+
+-- TREE TESTS
+data Tree a = Leaf | Branch a (Tree a) (Tree a)
+
+foldTree :: (a -> a -> r) -> r -> Tree a -> r
+foldTree _ init Leaf = r
+foldTree f init (Branch a l r) = f a $ f (foldTree f init l) (foldTree f init r)
+
 main :: IO ()
 main = hspec spec
 
